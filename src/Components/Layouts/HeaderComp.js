9e9241593d453d0bca92f6fd9362b92838374890
Harshada5686 <harshadawagh5686@gmail.com>
2025-05-05 14:06:38 +0530

    Add sign-in and sign-up form

--- src/Components/Layouts/HeaderComp.js
+++ src/Components/Layouts/HeaderComp.js
@@ -28,12 +28,98 @@ const HeaderComp = () => {
                         <Nav.Link href="#news" className="navitem">News</Nav.Link>
 
                         <div className="d-flex flex-column flex-lg-row ms-2">
-                            <button type="button" className="btn me-lg-4 mb-2 mb-lg-0">
-                            Sign in
+                            <button type="button" className="btn me-lg-4 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#loginModal">
+                                Sign in
                             </button>
-                            <button type="button" className="btn">
-                                Sign up
-                            </button>
+
+
+                            {/* sign in modal */}
+                            <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="login.php" class="form" method="POST">
+                                                <div class="mb-3">
+                                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+
+                                                    <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                                                        aria-describedby="emailHelp" />
+
+
+                                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" />
+
+
+                                                </div>
+                                                <div class="mb-3 form-check">
+                                                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                                </div>
+                                                <button type="submit" class="btn btn-primary" value="login">Login</button>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#signupModal"
+                        name="SIGNUP">SignUp</button>
+
+                            {/* Signup modal */}
+                            <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="signupModalLabel">Sign up</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" class="form" method="POST">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail2" class="form-label" name="email">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail2"
+                                aria-describedby="emailHelp"/>
+                            
+
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password" id="exampleInputPassword2"/>
+                           
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="cexampleInputPassword1" class="form-label">Confirm Password</label>
+                            <input type="password" name="confirmpassword" class="form-control"
+                                id="cexampleInputPassword1"/>
+                           
+
+                        </div>
+
+                        <button type="submit" class="btn btn-secondary" value="signup">Create Account</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
                         </div>
 
                     </Nav>
